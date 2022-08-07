1dd2de6a2c58cea4f6525cca3bc0bf9991dd8954
fernando <fernandoreal.valentim@gmail.com>
2022-08-07 00:28:53 -0300

    Fix: general styles

--- ui.frontend/src/components/Checkbox/Checkbox.js
+++ ui.frontend/src/components/Checkbox/Checkbox.js
@@ -1,24 +1,29 @@
 import React from "react";
 import { MapTo } from "@adobe/aem-react-editable-components";
 import { CheckBoxContainer, CheckboxInput } from "./CheckBox.styled";
+import { useState } from "react";
+
 import Label from "../Micros/Label/Label";
 
-export const Checkbox = ({ type, txtColor, inputColor, text, number }) => {
+export const Checkbox = ({ type , txtColor, inputColor, text, number }) => {
+  const [checked, setChecked] = useState(true)
+  const handleClick = () => setChecked(!checked)
+
   const defaulttxt = "Insert checkbox text"
-  if(type) {
-    return (
-      <CheckBoxContainer>
-        <CheckboxInput color={inputColor} type={"checkbox"} id={"checkbox"} checked/>
-        <Label text={text ? text : defaulttxt} color={txtColor} fontSize={number} />
-      </CheckBoxContainer>
-    );
-  } else {
-    return (
-      <CheckBoxContainer>
-        <CheckboxInput color={inputColor} type={"checkbox"} id={"checkbox"} />
-        <Label  text={text ? text : defaulttxt} color={txtColor} fontSize={number} />
-      </CheckBoxContainer>
-    );
-  }
+    if(type) {
+      return (
+        <CheckBoxContainer >
+          <CheckboxInput  onClick={handleClick} color={inputColor} type={"checkbox"} id={"checkbox"} checked={checked}/>
+          <Label text={text ? text : defaulttxt} color={txtColor} fontSize={number} />
+        </CheckBoxContainer>
+      );
+    } else {
+      return (
+        <CheckBoxContainer >
+          <CheckboxInput  onClick={handleClick} color={inputColor} type={"checkbox"} id={"checkbox"}/>
+          <Label text={text ? text : defaulttxt} color={txtColor} fontSize={number} />
+        </CheckBoxContainer>
+      );
+    }
 };
 export default MapTo("reactapp/components/checkbox")(Checkbox);
--- ui.frontend/src/components/Checkbox/Checkbox.styled.js
+++ ui.frontend/src/components/Checkbox/Checkbox.styled.js
@@ -1,14 +1,12 @@
 import styled from "styled-components";
 
 export const CheckBoxContainer = styled.div`
-    margin: 1rem;
-    display: flex;
-`
+  margin: 1rem;
+  display: flex;
+`;
 
 export const CheckboxInput = styled.input`
-    margin: 0.75rem;
-    width: 1.25rem;
-    accent-color: ${(props) => props.color};
-`
-
-
+  margin: 0.75rem;
+  width: 1.25rem;
+  accent-color: ${(props) => props.color};
+`;
