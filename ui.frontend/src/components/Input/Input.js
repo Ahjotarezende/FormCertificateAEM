ab1341988ba1380ac2e1698a63c89c8cb3fb2787
Victor Hugo <victor.vivtorhugo@gmail.com>
2022-08-09 13:24:48 -0300

    Feat:input component

--- ui.frontend/src/components/Input/Input.js
+++ ui.frontend/src/components/Input/Input.js
@@ -1,53 +1,74 @@
 import React, { useState } from "react";
-import { Container, ContainerInput } from "./Input.styled";
-function Input({ type, color, fontSize }) {
-  const [name, setName] = useState("");
+import {
+  Container,
+  ContainerInput,
+  ContainerInputBirthday,
+  ContainerInputPhone,
+} from "./Input.styled";
+function Input({ type, placeholder, inputName, color, fontSize }) {
+  const [age, setAge] = useState(50);
   const getTypeInput = (type) => {
     switch (type) {
-      case "name":
+      case "text":
         return (
           <Container>
             <ContainerInput
-              placeholder='Fullname'
+              placeholder={placeholder}
               type='text'
+              fontSize={fontSize ? fontSize : "16px"}
+              color={color ? color : "#aaaaaa"}
               onChange={(e) => {
-                setName(e.target.value);
+                localStorage.setItem(`${inputName}`, e.target.value);
+              }}
+            />
+          </Container>
+        );
+      case "number":
+        return (
+          <Container>
+            <ContainerInputPhone
+              placeholder={placeholder}
+              type='text'
+              maxLength='11'
+              font-size={fontSize ? fontSize : "16px"}
+              color={color ? color : "#aaaaaa"}
+              onChange={(e) => {
+                localStorage.setItem(`${inputName}`, e.target.value);
               }}
             />
           </Container>
         );
       case "email":
-        return <Container>
-        <ContainerInput
-          placeholder='Fullname'
-          type='text'
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-      </Container>;
-      case "number":
-        return <Container>
-        <ContainerInput
-          placeholder='Fullname'
-          type='text'
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-      </Container>;
-      case "text":
-        return <Container>
-        <ContainerInput
-          placeholder='Fullname'
-          type='text'
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-      </Container>;
+        return (
+          <Container>
+            <ContainerInput
+              placeholder={placeholder}
+              type='email'
+              color={color ? color : "#aaaaaa"}
+              font-size={fontSize ? fontSize : "16px"}
+              onChange={(e) => {
+                localStorage.setItem(`${inputName}`, e.target.value);
+              }}
+            />
+          </Container>
+        );
+      case "birthday":
+        return (
+          <Container>
+            <ContainerInputBirthday
+              type='number'
+              font-size={fontSize ? fontSize : "16px"}
+              color={color ? color : "#aaaaaa"}
+              value={age}
+              disabled
+              onChange={(e) => {
+                localStorage.setItem(`${inputName}`, e.target.value);
+              }}
+            />
+          </Container>
+        );
       default:
-        return <Container></Container>;
+        return <Container>set input</Container>;
     }
   };
   return <div>{getTypeInput(type)}</div>;
--- ui.frontend/src/components/Input/Input.styled.js
+++ ui.frontend/src/components/Input/Input.styled.js
@@ -1,10 +1,54 @@
 import styled from "styled-components";
-
+import { toRem } from "../../utils/convertToRem";
 export const Container = styled.div`
   display: flex;
-  width: 15vw;
+  width: 50vw;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  flex: 1;
+  @media (max-width: 768px) {
+    height: 4.5rem;
+  }
 `;
 
 export const ContainerInput = styled.input`
+  border: 0.125rem solid ${(props) => props.color};
+  box-sizing: border-box;
+  border-radius: 4px;
+  height: 2rem;
+  padding: 1rem 0.5rem;
+  margin-bottom: 1.5rem;
   width: 100%;
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+`;
+
+export const ContainerInputBirthday = styled.input`
+  border: 0.125rem solid ${(props) => props.color};
+  box-sizing: border-box;
+  border-radius: 4px;
+  height: 2rem;
+  padding: 1rem 0.5rem;
+  margin-bottom: 1.5rem;
+  width: 20%;
+
+  @media (max-width: 768px) {
+    width: 60%;
+  }
+`;
+
+export const ContainerInputPhone = styled.input`
+  border: 0.125rem solid ${(props) => props.color};
+  box-sizing: border-box;
+  border-radius: 4px;
+  height: 2rem;
+  padding: 1rem 0.5rem;
+  margin-bottom: 1.5rem;
+  width: 50%;
+
+  @media (max-width: 768px) {
+    width: 60%;
+  }
 `;
