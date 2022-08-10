429d83046f48df7a0ba7e7b3b4b9aaac6bd35f38
fernando <fernandoreal.valentim@gmail.com>
2022-08-10 11:16:00 -0300

    Fix: emproved logic and styles

--- ui.frontend/src/components/Checkbox/Checkbox.js
+++ ui.frontend/src/components/Checkbox/Checkbox.js
@@ -7,23 +7,13 @@ import Label from "../Micros/Label/Label";
 
 export const Checkbox = ({ type , txtColor, inputColor, text, number }) => {
   const [checked, setChecked] = useState(true)
-  const handleClick = () => setChecked(!checked)
 
-    if(type) {
-      return (
-        <CheckBoxContainer role="checkbox" onClick={handleClick} tabindex="0" title={text}>
-          <CheckboxInput color={inputColor} type={"checkbox"} id={"checkbox"} title={text} aria-label={text} aria-checked={checked} checked={checked}/>
-          <Label text={text} color={txtColor} fontSize={number} />
-        </CheckBoxContainer>
-      );
-    } else {
-      return (
-        <CheckBoxContainer role="checkbox" onClick={handleClick} tabindex="0" title={text}>
-          <CheckboxInput   color={inputColor} type={"checkbox"} id={"checkbox"} title={text} aria-label={text} aria-checked={checked} />
-          <Label text={text} color={txtColor} fontSize={number} />
-        </CheckBoxContainer>
-      );
-    }
+  return (
+    <CheckBoxContainer  role="checkbox" tabindex="0" title={text} onClick={() => setChecked(current => !current)} aria-label={type? checked : !checked}>
+      <CheckboxInput color={inputColor} type={"checkbox"}  id={"checkbox"} title={text} aria-label={text}  checked={type? checked : !checked} />
+      <Label text={text} color={txtColor} fontSize={number} />
+    </CheckBoxContainer>
+  );
 };
 
 
--- ui.frontend/src/components/Checkbox/Checkbox.styled.js
+++ ui.frontend/src/components/Checkbox/Checkbox.styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const CheckBoxContainer = styled.div`
   margin: 1rem;
   display: flex;
+  max-width: 36rem;
 `;
 
 export const CheckboxInput = styled.input`
