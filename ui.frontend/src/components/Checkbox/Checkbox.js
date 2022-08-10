3960589c592ee441ea42778dda20a2e71206faa2
fernando <fernandoreal.valentim@gmail.com>
2022-08-10 08:55:09 -0300

    Fix: add default props

--- ui.frontend/src/components/Checkbox/Checkbox.js
+++ ui.frontend/src/components/Checkbox/Checkbox.js
@@ -9,21 +9,26 @@ export const Checkbox = ({ type , txtColor, inputColor, text, number }) => {
   const [checked, setChecked] = useState(true)
   const handleClick = () => setChecked(!checked)
 
-  const defaulttxt = "Insert checkbox text"
     if(type) {
       return (
         <CheckBoxContainer >
           <CheckboxInput  onClick={handleClick} color={inputColor} type={"checkbox"} id={"checkbox"} checked={checked}/>
-          <Label text={text ? text : defaulttxt} color={txtColor} fontSize={number} />
+          <Label text={text} color={txtColor} fontSize={number} />
         </CheckBoxContainer>
       );
     } else {
       return (
         <CheckBoxContainer >
           <CheckboxInput  onClick={handleClick} color={inputColor} type={"checkbox"} id={"checkbox"}/>
-          <Label text={text ? text : defaulttxt} color={txtColor} fontSize={number} />
+          <Label text={text} color={txtColor} fontSize={number} />
         </CheckBoxContainer>
       );
     }
 };
+
+
+Checkbox.defaultProps = {
+  text: "Insert Text Here"
+}
+  
 export default MapTo("reactapp/components/checkbox")(Checkbox);
