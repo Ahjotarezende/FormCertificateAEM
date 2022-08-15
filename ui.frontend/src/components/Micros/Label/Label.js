a1785d71df89cef00d4defe0a4c4a15f493763dc
Victor Hugo <victor.vivtorhugo@gmail.com>
2022-08-15 00:33:43 -0300

    Feat: Component Input + Label + ErrorMessage

--- ui.frontend/src/components/Micros/Label/Label.js
+++ ui.frontend/src/components/Micros/Label/Label.js
@@ -3,8 +3,16 @@ import { LabelText } from "./Label.styled";
 
 function Label({ text, color, fontSize }) {
   return (
-    <LabelText color={color} fontSize={fontSize}>{text}</LabelText>
+    <LabelText color={color} fontSize={fontSize}>
+      {text}
+    </LabelText>
   );
 }
+
+Label.defaultProps = {
+  color: "#000",
+  fontSize: "14px",
+  text: "Insert label",
+};
 
 export default Label;
--- ui.frontend/src/components/Micros/Label/Label.styled.js
+++ ui.frontend/src/components/Micros/Label/Label.styled.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const LabelText = styled.p`
-  color:  ${(props) => props.color};
-  font-size: ${(props) => props.fontSize}px;
+  color: ${(props) => props.color};
+  font-size: ${(props) => props.fontSize};
+  margin: 0 2rem 0 0;
 `;
