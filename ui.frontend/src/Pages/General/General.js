5dfcea7064a0f4c2eb98ded3e9fbfd255fc5d1ca
fernando <fernandoreal.valentim@gmail.com>
2022-08-17 21:04:39 -0300

    Fix: general page fix

--- ui.frontend/src/Pages/General/General.js
+++ ui.frontend/src/Pages/General/General.js
@@ -41,42 +41,124 @@ const General = (props) => {
 
   const { headerresult, colorresult, buttonresult } = props;
   return (
-    <FullPage
-        fontFamily={fonts}
-        backGround={bgpage}
-        id="FullPage"
-    >
-        
-
-        <Social
-          headersocial={headersocial}
-          navsocial={navsocial}
-          inputsocial={inputsocial}
-          birthsocial={birthsocial}
-          checksocial={checksocial}
-          buttonsocial={buttonsocial}
-          certsocial={certsocial}
-        />
-
-        <Basic
-          headerbasic={headerbasic}
-          navbasic={navbasic}
-          inputbasic={inputbasic}
-          birthbasic={birthbasic}
-          checkbasic={checkbasic}
-          buttonbasic={buttonbasic}
-          certbasic={certbasic}
-        />
-        <Certificate fonts={fonts} title={title} titlecolor={titlecolor} fonte={fstitle} bgform={bgform}
-          colorcomp={colorcomp} headercertificate={headercertificate} navcertificate={navcertificate} inputcertificate={inputcertificate}
-          birthcertificate={birthcertificate} checkcertificate={checkcertificate} buttoncertificate={buttoncertificate}
-        certcertificate={certcertificate}/> 
-        
+    <FullPage fontFamily={fonts} backGround={bgpage} id='FullPage'>
+      <Basic
+        headerbasic={headerbasic}
+        navbasic={navbasic}
+        inputbasic={inputbasic}
+        birthbasic={birthbasic}
+        checkbasic={checkbasic}
+        buttonbasic={buttonbasic}
+        certbasic={certbasic}
+      />
+      <Certificate
+        fonts={fonts}
+        title={title}
+        titlecolor={titlecolor}
+        fonte={fstitle}
+        bgform={bgform}
+        colorcomp={colorcomp}
+        headercertificate={headercertificate}
+        navcertificate={navcertificate}
+        inputcertificate={inputcertificate}
+        birthcertificate={birthcertificate}
+        checkcertificate={checkcertificate}
+        buttoncertificate={buttoncertificate}
+        certcertificate={certcertificate}
+      />
     </FullPage>
   );
 };
 
 General.defaultProps = {
+  headerbasic: [
+    {
+      smalltextbs: "Basic",
+      bigtextbs: "Basic",
+      colorbigtextbs: "#000000",
+      colorsmalltextbs: "red",
+    },
+  ],
+  navbasic: [
+    {
+      textonebs: "First Tab",
+      texttwobs: "Second Tab",
+      textthreebs: "Third Tab",
+      colortextnavbs: "#000000",
+      colorselectedbs: "red",
+    },
+  ],
+  inputbasic: [
+    {
+      textonebs: "",
+      texttwobs: "",
+      typeinputbs: "",
+      textthreebs: "",
+      colortextnavbs: "",
+      colorselectedbs: "",
+      colorcompbs: "",
+      requiredinputbs: {},
+    },
+  ],
+  birthbasic: [
+    {
+      startbirthbs: "",
+      endbirthbs: "",
+      colorbirthbs: "#aaaaaa",
+      labelbirthbs: "Set Birthday",
+    },
+  ],
+  checkbasic: [
+    {
+      textcheckbs: "set checkbox",
+      errorcheckbs: "",
+      colorcheckbs: "",
+      colortextcheckbs: "",
+      colorerrorcheckbs: "",
+      fscheckbs: "",
+      checkbs: "",
+      roundbs: "",
+      requiredcheckbs: "",
+    },
+  ],
+  buttonbasic: [
+    {
+      leftimagebs: "https://via.placeholder.com/150",
+      rightimagebs: "https://via.placeholder.com/150",
+      fsbuttonbs: "16",
+      brbuttonbs: "2",
+      txtbuttonbs: "Button",
+      colortxtbuttonbs: "#000000",
+      colorbuttonbs: "#ffffff",
+      onClick: () => {},
+      form: "",
+      type: "submit",
+    },
+  ],
+  certbasic: [
+    {
+      labelcertbs: "sdfsdf",
+      colorlabelcertbs: "",
+      colorbtbs: "",
+      leftimagecertbs: "",
+      rightimagecertbs: "",
+      txtcertbs: "",
+      colortxtcertbs: "",
+      fstxtcertbs: "",
+      brbtcertbs: "",
+      bgopcertbs: "",
+      colorbtmorebs: "",
+      leftimagemorebs: "",
+      rightimagemorebs: "",
+      txtmorebs: "",
+      colortxtmorebs: "",
+      fstxtmorebs: "",
+      brbtmorebs: "",
+      colorerrorcertbs: "",
+      phcertbs: "",
+      errortextbs: "",
+    },
+  ],
   headersocial: [
     {
       smalltextsc: "Forms",
@@ -94,8 +176,6 @@ General.defaultProps = {
       colorselectedsc: "#0000FF",
     },
   ],
- 
-
 
   birthsocial: [
     {
@@ -123,51 +203,46 @@ General.defaultProps = {
       fsbuttonsc: 16,
       brbuttonsc: 4,
       leftimagesc: {
-        src: ""
+        src: "",
       },
       rightimagesc: {
-        src: ""
-      }
-    }
-   ],
-
-   inputsocial: [
-    {
-      
+        src: "",
+      },
     },
   ],
 
+  inputsocial: [{}],
 
   certsocial: [
-      {
-          colorbtsc: "#074EE8",
-          leftimagecertsc: {
-              src: ""
-          },
-          rightimagecertsc: {
-              src: "https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/48/FFFFFF/external-Down-arrows-tanah-basah-glyph-tanah-basah-5.png"
-          },
-          txtcertsc: "Certificates",
-          colortxtcertsc: "#FFFFFF",
-          fstxtcertsc: 16,
-          brbtcertsc: 4,
-          bgopcertsc: "#074EE8",
-          colorbtmoresc: "#074EE8",
-          leftimagemoresc: {
-              src: "https://img.icons8.com/ios-glyphs/30/FFFFFF/plus-math.png"
-          },
-          rightimagemoresc: {
-              src: "https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/48/FFFFFF/external-Right-arrows-tanah-basah-glyph-tanah-basah-3.png"
-          },
-          txtmoresc: "More",
-          colortxtmoresc: "#FFFFFF",
-          fstxtmoresc: 16,
-          brbtmoresc: 4,
-          colorerrorcertsc: "#FF0000",
-          phcertsc: "https://www.linkedin.com/in/foo-bar-3a0560104/",
-          errortextsc: "Empty certificate is not allowed."
-      }
-  ]
+    {
+      colorbtsc: "#074EE8",
+      leftimagecertsc: {
+        src: "",
+      },
+      rightimagecertsc: {
+        src: "https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/48/FFFFFF/external-Down-arrows-tanah-basah-glyph-tanah-basah-5.png",
+      },
+      txtcertsc: "Certificates",
+      colortxtcertsc: "#FFFFFF",
+      fstxtcertsc: 16,
+      brbtcertsc: 4,
+      bgopcertsc: "#074EE8",
+      colorbtmoresc: "#074EE8",
+      leftimagemoresc: {
+        src: "https://img.icons8.com/ios-glyphs/30/FFFFFF/plus-math.png",
+      },
+      rightimagemoresc: {
+        src: "https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/48/FFFFFF/external-Right-arrows-tanah-basah-glyph-tanah-basah-3.png",
+      },
+      txtmoresc: "More",
+      colortxtmoresc: "#FFFFFF",
+      fstxtmoresc: 16,
+      brbtmoresc: 4,
+      colorerrorcertsc: "#FF0000",
+      phcertsc: "https://www.linkedin.com/in/foo-bar-3a0560104/",
+      errortextsc: "Empty certificate is not allowed.",
+    },
+  ],
 };
 
 export default MapTo("reactapp/components/geralpage")(General);
