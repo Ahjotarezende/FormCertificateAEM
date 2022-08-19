4e17a544db8b3331106bda6df04541cab16490aa
fernando <fernandoreal.valentim@gmail.com>
2022-08-19 10:28:40 -0300

    Small fix

--- ui.frontend/src/Pages/Social/Social.Styled.js
+++ ui.frontend/src/Pages/Social/Social.Styled.js
@@ -5,14 +5,44 @@ export const Title = styled.p`
     font-weight: 600;
     font-size: ${(props) => props.fonte};
     color: ${(props) => props.titlecolor};
+    margin-bottom: 33px;
+    @media (max-width: 768px) {
+        margin-top: 40px;
+  }
+  @media (max-width: 415px){
+        margin-bottom: 25px;
+    }
 `;
 
 export const DivForm = styled.div`
     background-color: ${(props) => props.bgform};
-    width: 600px;
-    height: 610px;
-   
-
+    box-sizing: border-box;
+    width: 617px;
+    height: 733px;
+    .none{
+        display: none;
+    }
     border-radius: 20px;
-    margin: 10px auto;
+    margin: 0 auto;
+    padding: 40px 35px 0px 67px;
+    display: flex;
+    flex-direction: column;
+    form{
+        display: flex;
+        flex-direction: column;
+        button{
+            align-self: flex-end;
+        }
+    }
+    
+    @media (max-width: 768px) {
+        width: 100vw;
+        padding: 0;
+        height: 93vh;
+        border-radius: 0;
+        padding: 0 30px;
+  }
+    @media (max-width: 415px){
+        height: 100vh;
+    }
 `;
